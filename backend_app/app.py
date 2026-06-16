c5c9c099ba86ae23a1c12230753e230dcf34eb35
by-sdudent <ia04kuchynskyiboryslav@gmail.com>
2026-06-16 20:30:21 +0200

    added simple metric and /metric endpoint

--- backend_app/app.py
+++ backend_app/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
 
 app = Flask(__name__)
 
+c = Counter('http_requests_on_root_page', 'Number of visits for main page')
+
+
 @app.route('/')
 def hello_world():
+    c.inc()
     return 'Hello World'
+
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run()
+
+
