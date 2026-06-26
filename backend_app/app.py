fa6f97b02bcf112a3ba735340b633f5f81b93953
by-sdudent <ia04kuchynskyiboryslav@gmail.com>
2026-06-26 10:27:59 +0200

    added /api/pod-info

--- backend_app/app.py
+++ backend_app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import os
 
 
 app = Flask(__name__)
@@ -16,6 +17,18 @@ def hello_world():
 @app.route('/metrics')
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+@app.route('/api/pod-info')
+def pod_info():
+    pod_name = os.environ.get("POD_NAME")
+    node_name = os.environ.get("NODE_NAME")
+    pod_ip = os.environ.get("POD_IP")
+    return jsonify({
+    "pod_name": pod_name,
+    "node_name": node_name,
+    "pod_ip": pod_ip
+    })
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
---
+++ backend_app/readme.md
@@ -0,0 +1,8 @@
+## Backend
+To build docker image:
+
+```bash
+docker build -t  bysdudent/flask-backend:0.2 .
+```
+
+
